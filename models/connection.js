1301c30b02375da5db837cca5430298906b97a29
craftByElla <ella.thomas.mail@gmail.com>
2024-05-23 11:14:12 +0200

    smiley

--- models/connection.js
+++ models/connection.js
@@ -3,5 +3,5 @@ const mongoose = require('mongoose');
 const connectionString = process.env.CONNECTION_STRING;
 
 mongoose.connect(connectionString, { connectTimeoutMS: 2000 })
-  .then(() => console.log(`Freedent Refreshers`))
+  .then(() => console.log(`🏀🦄 Freedent Refreshers 🍱🗽`))
   .catch(error => console.error(error));
